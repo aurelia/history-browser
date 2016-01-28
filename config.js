44d69dbce9495a783771fb1d8659b7226cb82400
Rob Eisenberg <rob@bluespire.com>
2016-01-28 17:24:05 -0500

    chore(package): update for jspm; core-js; aurelia deps

--- config.js
+++ config.js
@@ -14,11 +14,11 @@ System.config({
   },
 
   map: {
-    "aurelia-history": "npm:aurelia-history@1.0.0-beta.1",
-    "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1",
+    "aurelia-history": "npm:aurelia-history@1.0.0-beta.1.1.1",
+    "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.1.1",
     "babel": "npm:babel-core@5.1.13",
     "babel-runtime": "npm:babel-runtime@5.1.13",
-    "core-js": "npm:core-js@1.2.6",
+    "core-js": "npm:core-js@2.0.3",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
@@ -34,7 +34,7 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:core-js@1.2.6": {
+    "npm:core-js@2.0.3": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2",
